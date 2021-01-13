8abc45dedde52d4e7c57ee6ab4faefc5ce5909f0
Sandra Spanikova <spanikovasandra@gmail.com>
2021-01-13 14:10:26 +0100

    add routes: company

---
+++ routes/companies.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const ExpressError = require("../expressError");
+const router = express.Router();
+const db = require("../db");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const results = await db.query(`SELECT * FROM companies`);
+    return res.json({ companies: results.rows });
+  } catch (e) {
+    return next(e);
+  }
+});
+
+router.get("/:code", async (req, res, next) => {
+  try {
+    const { code } = req.params;
+    const results = await db.query(`SELECT * FROM companies WHERE code=$1`, [
+      code,
+    ]);
+    if (results.rows.length === 0) {
+      throw new ExpressError(`Can't find company with code of ${code}`, 404);
+    }
+    return res.json({ company: results.rows[0] });
+  } catch (e) {
+    return next(e);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { code, name, description } = req.body;
+    const results = await db.query(
+      `INSERT INTO companies (code, name, description) VALUES ($1, $2, $3) RETURNING *`,
+      [code, name, description]
+    );
+    return res.status(201).json({ company: results.rows[0] });
+  } catch (e) {
+    return next(e);
+  }
+});
+
+module.exports = router;

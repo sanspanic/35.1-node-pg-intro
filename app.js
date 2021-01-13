593de693a3e7b45d70e39a413565bc54eaab885d
Sandra Spanikova <spanikovasandra@gmail.com>
2021-01-13 15:52:13 +0100

    add routes: invoices

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const ExpressError = require("./expressError");
 
 app.use(express.json());
 const companyRoutes = require("./routes/companies");
+const invoiceRoutes = require("./routes/invoices");
+
 app.use("/companies", companyRoutes);
+app.use("/invoices", invoiceRoutes);
 
 /** 404 handler */
 
--- routes/companies.js
+++ routes/companies.js
@@ -15,13 +15,21 @@ router.get("/", async (req, res, next) => {
 router.get("/:code", async (req, res, next) => {
   try {
     const { code } = req.params;
-    const results = await db.query(`SELECT * FROM companies WHERE code=$1`, [
+    const company = await db.query(`SELECT * FROM companies WHERE code=$1`, [
       code,
     ]);
-    if (results.rows.length === 0) {
+    if (company.rows.length === 0) {
       throw new ExpressError(`Can't find company with code of ${code}`, 404);
     }
-    return res.json({ company: results.rows[0] });
+
+    const invoices = await db.query(
+      `SELECT * FROM invoices WHERE comp_code=$1`,
+      [code]
+    );
+
+    company.rows[0].invoices = invoices.rows;
+
+    return res.json({ company: company.rows[0] });
   } catch (e) {
     return next(e);
   }
